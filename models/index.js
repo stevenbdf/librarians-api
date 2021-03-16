c72dd40dd7d5f5480a351666d15d8c8d32d0c282
Steven Diaz <stevenbdf@gmail.com>
2021-03-15 23:53:11 -0600

    🔨 Use Central America Time Zone

--- models/index.js
+++ models/index.js
@@ -4,13 +4,23 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(config.dbName, config.dbUser, config.dbPassword, {
   host: config.dbHost,
   dialect: config.dbDialect,
+  // to return UTC time, remove this property
+  dialectOptions: {
+    typeCast: function (field, next) { // for converting to time
+      if (field.type === 'DATETIME') {
+        return field.string()
+      }
+      return next()
+    },
+  },
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
     idle: 10000
   },
-  logging: false
+  logging: false,
+  timezone: '-06:00'
 });
 
 const db = {};
